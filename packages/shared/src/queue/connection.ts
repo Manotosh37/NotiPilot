c022e9db1d20e57e2c5fecae2960ff87ff64f49c
Manotosh3 <manotoshphade3@gmail.com>
2026-01-19 00:47:18 +0530

    Redis & bullMq

--- packages/shared/src/queue/connection.ts
+++ packages/shared/src/queue/connection.ts
@@ -1,5 +1,5 @@
 export const bullmqConnection = {
     host: process.env.REDIS_HOST ?? "localhost",
-    port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT): "6379",
+    port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT): 6379,
     password: process.env.REDIS_PASSWORD || undefined
 }
