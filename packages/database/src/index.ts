e433b020e2954c2a2e6c564e5089bf1ab39ac105
Manotosh3 <manotoshphade3@gmail.com>
2026-01-18 19:58:08 +0530

    Database

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -2,9 +2,3 @@ export { connectDB } from "./connection.js"
 export { notiRepo } from "./repos/noti.repo.js"
 export * from "./models/noti.mod.js"
 export * from "./repos/noti.repo.js"
-
-
-dfgbsrdfgjbnhdfsuijlbndgfsjkbvnmdfgsgfbmfgkbmgf,b vcmn
-findPackageJSON;waitForDebugger,kfgdbmfgdbdfg
-fdbm,gdfkbmfgblfgbmfdklbmgfdlb,debugger\
-;df,bldfg,bdfl;b
