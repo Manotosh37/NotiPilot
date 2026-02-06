d805b9281cafd6cf11c77c091eaa57da78dc9500
Manotosh3 <manotoshphade3@gmail.com>
2026-02-06 17:42:13 +0530

    initialize notification worker

---
+++ apps/worker/src/index.ts
@@ -0,0 +1,14 @@
+import { Worker } from "bullmq"
+import { QUEUE_NAME, bullmqConnection } from "@Notipilot/packages/shared"
+
+new Worker("notifications", async (job) => {
+    console.log(job.data.notificationId)
+})
+
+new Worker( QUEUE_NAME.NOTIFICATION_DELIVERY, async (job) =>{
+    const { notificationId } = job.data;
+    console.log("Received job for notification:", notificationId)
+},
+{
+    connection: bullmqConnection
+})
