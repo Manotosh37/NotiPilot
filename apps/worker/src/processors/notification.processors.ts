cd2e7a65498d0e41fe4ff66b5ffe7c0f9aa2debd
Manotosh3 <manotoshphade3@gmail.com>
2026-02-19 23:59:03 +0530

    providers1

--- apps/worker/src/processors/notification.processors.ts
+++ apps/worker/src/processors/notification.processors.ts
@@ -1,16 +1,32 @@
-import { notiRepo, notiChannel } from "@notipilot/database"
-import { sendEmail } from "../providors/email.provider";
+import { notiRepo } from "@notipilot/database"
+import { NotiChannel } from "@notipilot/database"
+import { sendEmail } from "../providers/email.provider";
+
 export async function processNotification(notificationId: string) {
     const notification = await notiRepo.getById(notificationId)
 
     if (!notification) {
-        return notification
+        return 
     }
 
     await notiRepo.markAsProcessing(notificationId);
-    if (notification.channel === notiChannel.sendEmail) {
+    if (notification.channel === NotiChannel.EMAIL) {
         console.log("Sending mail:")
-    } else if (notification.channel === notiChannel.SMS) {
+        const { to, subject, body} = notification.payload as {
+            to: string,
+            subject: string,
+            body: string
+        }
+        try {
+            await sendEmail(to, subject, body)
+            await notiRepo.markAsSent(notificationId) }
+
+        catch (error) {
+            await notiRepo.incrementRetry(notificationId)
+            await notiRepo.markAsFailed(notificationId)
+            throw error}
+    } 
+    else if (notification.channel === NotiChannel.SMS) {
         console.log("Sending SMS:")
     }
 } 
